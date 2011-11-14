6e5f41c3b0df0d2af03d73c2c7a82b96eb57239e
mail2naresh@gmail.com <mail2naresh@gmail.com@179d32ca-b5b1-e252-8014-6139ce2a436f>
2011-11-14 04:51:23 +0000

    Removing dendogram
    git-svn-id: https://cse592-project.googlecode.com/svn@17 179d32ca-b5b1-e252-8014-6139ce2a436f

--- trunk/src/dendogram.py
+++
@@ -1,8 +0,0 @@
-
-class dendogram:
-    def __init__(self, ft_vector, inst_vectors):
-        self.ft_vector  = ft_vector
-        self.inst_vectors   = inst_vectors
-
-    def maketree(self):
-        pass
