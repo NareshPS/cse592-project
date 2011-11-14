ab5fce7c0bd8fb098dd162b5e7585cb9351c4176
mail2naresh@gmail.com <mail2naresh@gmail.com@179d32ca-b5b1-e252-8014-6139ce2a436f>
2011-11-14 02:19:59 +0000

    Adding more files
    git-svn-id: https://cse592-project.googlecode.com/svn@15 179d32ca-b5b1-e252-8014-6139ce2a436f

---
+++ trunk/src/dendogram.py
@@ -0,0 +1,8 @@
+
+class dendogram:
+    def __init__(self, ft_vector, inst_vectors):
+        self.ft_vector  = ft_vector
+        self.inst_vectors   = inst_vectors
+
+    def maketree(self):
+        pass
