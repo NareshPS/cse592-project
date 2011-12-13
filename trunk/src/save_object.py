f3938b175f49281f732b77bb575e5969b0a4c555
mail2naresh@gmail.com <mail2naresh@gmail.com@179d32ca-b5b1-e252-8014-6139ce2a436f>
2011-12-13 07:18:01 +0000

    save_object comments
    git-svn-id: https://cse592-project.googlecode.com/svn@32 179d32ca-b5b1-e252-8014-6139ce2a436f

--- trunk/src/save_object.py
+++ trunk/src/save_object.py
@@ -3,6 +3,12 @@ import os,pickle
 
 class save_object:
   def save_it(self, obj, store_name):
+    '''
+      Saves the object in a file. It uses python
+      pickle module to dump to disk.
+      obj         : Object to dump on disk.
+      store_name  : File to store the object.
+    '''
     if obj is not None and store_name is not None:
       fp  = open(store_name, 'w')
       pickle.dump(obj, fp)
@@ -11,6 +17,11 @@ class save_object:
       raise Exception('Null object or store name')
 
   def load_it(self, store_name):
+    '''
+      Loads the object from file. Returns the read object.
+      Uses pyhton pickle module to read from disk.
+      store_name    : File to read the object from.
+    '''
     if os.path.exists(store_name) is False:
       return None
     else:
