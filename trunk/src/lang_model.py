f3314e60f3c1239321f8394f876966d4a2e85a91
mail2naresh@gmail.com <mail2naresh@gmail.com@179d32ca-b5b1-e252-8014-6139ce2a436f>
2011-12-22 03:32:14 +0000

    Adding lang_model.py
    git-svn-id: https://cse592-project.googlecode.com/svn@59 179d32ca-b5b1-e252-8014-6139ce2a436f

---
+++ trunk/src/lang_model.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+from nltk import wordpunct_tokenize
+from nltk.model.ngram import NgramModel
+from nltk.probability import WittenBellProbDist
+from math import pow
+
+class model:
+  '''
+    This class builds language model for the supplied corpus.
+    It uses NLTK to build the model.
+  '''
+  def __init__(self):
+    pass
+  
+  def build_model(self, text):
+    c = wordpunct_tokenize(text)
+    e = lambda fdist, bins: WittenBellProbDist(fdist, len(fdist)+1)
+    m = NgramModel(1, c, e)
+    return m
+
+  def ppx(self, m, text):
+    '''
+      Returns the perplexity of text
+      in the given model.
+      
+      m     : model
+      text  : text
+    '''
+    c = wordpunct_tokenize(text)
+    print c
+    print 1/pow(m.entropy(c), 2.0)
+
+if __name__ == '__main__':
+  m   = model()
+  print m
